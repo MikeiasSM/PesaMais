74d9957c6e06573b9b07a58a1b67b73d5138915c
Mikeias Santos <mikeiassantos16@gmail.com>
2022-02-08 16:00:45 -0300

    Criação de DAOs de Persistência

---
+++ Model/Connection/PesaMais.Model.Connection.DmConnection.dfm
@@ -0,0 +1,14 @@
+object Connection: TConnection
+  Height = 164
+  Width = 228
+  PixelsPerInch = 96
+  object FDConnection: TFDConnection
+    Left = 48
+    Top = 56
+  end
+  object FDQuery: TFDQuery
+    Connection = FDConnection
+    Left = 128
+    Top = 56
+  end
+end
--- PesaMais.dpr
+++ PesaMais.dpr
@@ -8,12 +8,15 @@ uses
   PesaMais.Model.Usuario in 'Model\PesaMais.Model.Usuario.pas',
   PesaMais.Model.Estado in 'Model\PesaMais.Model.Estado.pas',
   PesaMais.Model.Cidade in 'Model\PesaMais.Model.Cidade.pas',
-  PesaMais.Model.Endereco in 'Model\PesaMais.Model.Endereco.pas';
+  PesaMais.Model.Endereco in 'Model\PesaMais.Model.Endereco.pas',
+  PesaMais.Model.Connection.DmConnection in 'Model\Connection\PesaMais.Model.Connection.DmConnection.pas' {Connection: TDataModule},
+  PesaMais.Model.Dao.DaoUsuario in 'Model\Dao\PesaMais.Model.Dao.DaoUsuario.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TConnection, Connection);
   Application.Run;
 end.
